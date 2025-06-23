261ca6ff945cb2aa93fe61471f76821f0f3e36de
tori <171421921+tokonatu55@users.noreply.github.com>
2025-06-23 21:00:45 +0900

    初期ファイル作成

---
+++ src/main/java/com/example/kakeibo_tool/KakeiboToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakeibo_tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakeiboToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakeiboToolApplication.class, args);
+	}
+
+}
